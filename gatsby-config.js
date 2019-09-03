650f63807aabe8b2a051577903c1c57fe173fbc5
Mart Molenaar <mart.molenaar@kpn.com>
2019-09-03 21:24:42 +0200

    first update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Millersdev`,
+    author: `Mart Molenaar`,
+    description: `Personal blog by Mart Molenaar.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `millersdev`,
     },
   },
   plugins: [
